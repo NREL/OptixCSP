379f8fbcdd46757df4ec33061f87df82657707cc
Taylor Brown <60201147+taylorbrown75@users.noreply.github.com>
2025-12-11 09:54:29 -0700

    Reorder hittype enum to align with SolTrace

--- src/core/soltrace_constants.h
+++ src/core/soltrace_constants.h
@@ -5,13 +5,16 @@
 namespace OptixCSP {
 
     // Hit type
+    // TODO: Replace this with RayEvent from simulation_result.hpp?
     enum HitType : uint8_t {
         HIT_UNASSIGNED = 0,
-        HIT_CREATE = 1,  // raygen
-        HIT_REFLECT = 2,
-        HIT_TRANSMIT = 3,
-        HIT_ABSORB = 4,
-        HIT_MISS = 5
+        HIT_CREATE = 1,
+        HIT_ABSORB = 2,
+        HIT_REFLECT = 3,
+        HIT_TRANSMIT = 4,
+        HIT_VIRTUAL = 5,
+        HIT_EXIT = 6,
+        HIT_UNKNOWN = 255
     };
 
     // Element ID consts
